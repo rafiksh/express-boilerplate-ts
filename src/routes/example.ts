22b1c518c37dfae190108129f6d7cacbe83319a9
Rafik <rafikshmoury@gmail.com>
2022-05-22 22:39:02 +0300

    feat(example-route): add put and delete routes

--- src/routes/example.ts
+++ src/routes/example.ts
@@ -1,12 +1,22 @@
 import { Router } from "express";
 
-import { addExample, getExamples, getExampleById } from "&controllers/example";
+import {
+  addExample,
+  getExamples,
+  getExampleById,
+  updateExample,
+  deleteExample,
+} from "&controllers/example";
 
 const exampleRoute = (router: Router) => {
   router.get("/", getExamples);
   router.get("/:id", getExampleById);
 
   router.post("/", addExample);
+
+  router.put("/:id", updateExample);
+
+  router.delete("/:id", deleteExample);
 
   return router;
 };
